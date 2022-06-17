92b0d1b4a07b8bb62ee07ecd2ae77d3048c0d5b4
rahmadxhidayat1 <sirlock1812@gmail.com>
2022-06-17 22:48:59 +0700

    error

--- admin/mod_blog/blogCtrl.php
+++ admin/mod_blog/blogCtrl.php
@@ -1,101 +1,130 @@
+latihan_webphpNw/admin/blog/blogctrl.php /
+@praks27
+praks27 update modal kores
+Latest commit 2e12f08 2 days ago
+ History
+ 1 contributor
+122 lines (110 sloc)  4.13 KB
+
 <?php 
 if(isset($_GET['act']) && ($_GET['act']=="update" || $_GET['act']== "save")){
+	//ketika code ini, posisi ada d folder mod_menu>admin>config
 	require_once "../../config/koneksi_db.php";
 	require_once "../../config/config.php";		
-}else{
+}
+else{
+	//ketika code ini, posisi ada d folder admin>config
 	require_once "../config/koneksi_db.php";
 	require_once "../config/config.php";
 }
-if(isset($_POST['btnupload'])){
-	$file= $_FILES['urlfile'];
-	var_dump($file);
-	//tentukan file folder lokasi penyimpamam file
-	$target_dir="../../assets/img/";
-	echo $file['name']."<br/>";
-	$target_file = $target_dir.$file['name'];
-	echo $target_file."<br/>";
-	move_uploaded_file($file['tmp_name'], $target_file);
-}
-if(isset($_GET['act']) && ($_GET['act']== "add")){	
+security_login();
+
+
+if(isset($_GET['act']) && ($_GET['act']== "add")){
+	//jika ada send variabel act=add, tampil form input/tambah
 	$judul = "Form Input Data";
+
 }
 else if(isset($_GET['act']) && ($_GET['act']== "edit")){
+	//jika ada send variabel act=edit, tampil form edit/ubah data
 	$judul = "Form Edit Data";
 	$idkey = $_GET['id']; //dapat dari URL
-	$qdata = mysqli_query($connect_db,"select * from mst_blog where id_blog=$idkey")or die(mysqli_error($connect_db));
+	$qdata = mysqli_query($connect_db,"select * from mst_blog where id_blog=$idkey")
+			or die(mysqli_error($connect_db));
 	$data = mysqli_fetch_array($qdata);
 }
 else if(isset($_GET['act']) && ($_GET['act']== "save")){
-	$juduls = $_POST['judul'];//$_POST untuk ngambil data yang menggunakan name(menggunakan php)
-	$kategori= $_POST['id_kategori'];
+
+	//jika ada send variabel act=save, ketika proses simpan(insert)
+	$judul = $_POST['judul'];
+	$kategori= $_POST['kategori'];
     $author=$_POST['author'];
     $isi=$_POST['isi'];
     $date=$_POST['date_input'];
-	$file = $_FILES['urlfile']; 
-	var_dump($file);	
-	/*tentukan folder lokasi direktori penyimpanan file */
-	$target_dir = "../../assets/img/";
-	//output ini yang disimpan ke database
-	/*tujuan penyimpanan file, direktori dan nama file*/
-	$target_file = $target_dir.$file['name']; 
-		//echo $target_file."<br/>";
-	/*untuk mendapatkan tipe file yang diupload */
-	$type_file = pathinfo($file['name'],PATHINFO_EXTENSION);
-	/*buat variabel untuk menampung hasil validasi ,
-	apakah file boleh diupload atau tidak, jika 1 maka boleh diupload,
-	jika 0 maka tidak dapat diupload*/	
-	$is_upload = 1;
-	/* cek batas limit file maks.3MB*/
-	if($file['size'] > 10000000){
-		$is_upload = 0;
-		// pesan("File lebih dari 1MB!!");		
-	}
-	/**cek tipe file */
-	if($type_file != "jpg"){
-		$is_upload = 0;
-		// pesan("Tipe file bukan file gambar!!");	
-	}
-	/**buat variabel untuk menampung nama file yang akan disimpan ke database,
-	 * dengan nilai awal kosong, akan di beri nilai jika upload berhasil
-	 */
-	$namafile = "";
-	/**proses upload */
-	if($is_upload == 1){
-		if(move_uploaded_file($file['tmp_name'], $target_file)){
-			$namafile = $file['name']; //variabel ini yang di panggil di query
-			mysqli_query($connect_db, 
-		"INSERT into mst_blog(judul,id_kategori,author,isi,date_input,gambar) VALUES('$juduls','$kategori','$author','$isi','$date','$namafile')")
-		or die (mysqli_error($connect_db));
-		header("Location: ../home.php?modul=mod_blog");
-			// pesan("Berhasil upload file gambar!!");	
-		}
-		else{
-			// pesan("GAGAL upload file gambar!!");	
-		}
+	// $img=$_POST['upload'];
+		 $img=$_FILES['upload'];
+		//  $file=$img['name'];
+		 $targetfol="../../assets/img/";
+		 $targetfile=$targetfol.$img['name'];
+		 $type_file=pathinfo($img['name'],PATHINFO_EXTENSION);
+
+		 $is_upload=0;
+
+		//  if($img['size'] > 1000000){
+		// 	$is_upload=0;
+		// 		header("Location: ../home.php?modul=blog&pesan=size");
+		//  }
+		//  if($type_file != "jpg"){
+		// 	$is_upload=0;
+		// 		header("Location: ../home.php?modul=blog&pesan=ekstensi");
+		//  }
+
+		 $namafile="";
+		//  if($is_upload==1){
+		// 	if(move_uploaded_file($img['tmp_name'],$targetfile)){
+		// 		$namafile=$file['name'];
+		// 		mysqli_query($connect_db,"INSERT into mst_blog(judul,id_kategori,author,isi,date_input,gambar) VALUES('$judul','$kategori','$author','$isi','$date','$file')")
+		// 			or die (mysqli_error($connect_db));
+		// 		header("Location:  ../home.php?modul=blog&pesan=sukses");
+		// 	}else{
+		// 		header("Location:  ../home.php?modul=blog&pesan=gagal");
+		// 	}
+		// }
+		//  $is_upload=1;
+		// 	if($img['size'] > 1000000){
+		// 		$is_upload=0;
+		// 		echo '<script>alert("ukuran file terlalu besar")</script>';
+		// 	}
+
+	//query untuk simpan
+	$qinsert = mysqli_query($connect_db, 
+			"INSERT into mst_blog(judul,id_kategori,author,isi,date_input,gambar) VALUES('$judul','$kategori','$author','$isi','$date','$file')")
+			or die (mysqli_error($connect_db));
+	if($qinsert){
+		//ketik proses simpan berhasil
+		header("Location: http://localhost/latihan_webphp1/admin/home.php?modul=blog");
 	}
 }
 else if(isset($_GET['act']) && ($_GET['act']== "update")){
+	//jika ada send variabel act=update, ketika proses simpan ubah data
 	$id = $_POST['id_blog'];
 	$up_judul = $_POST['up_judul'];
 	$up_kategori = $_POST['up_kategori'];
     $up_isi=$_POST['isi'];
     $up_author=$_POST['up_author'];
     $up_date=$_POST['date_input'];
+	$upgmbr=$data['gambar'];
+
+		if(isset($_POST['btnsimpan'])){
+			$upimg=$_FILES['upload'];
+			$upfile=$upimg['name'];
+			$uptargetfol="../../assets/img/";
+			$uptargetfile=$uptargetfol.$upimg['name'];
+			move_uploaded_file($upimg['tmp_name'],$uptargetfile);
+			
+	}
+
+	//query untuk simpan
 	$qinsert = mysqli_query($connect_db, 
-			"UPDATE mst_blog SET judul='$up_judul', id_kategori='$up_kategori', isi='$up_isi',author='$up_author',date_input='$up_date' WHERE id_blog='$id'")
+			"UPDATE mst_blog SET judul='$up_judul', id_kategori='$up_kategori', isi='$up_isi',author='$up_author',date_input='$up_date',gambar='$upfile 'WHERE id_blog='$id'")
 			or die (mysqli_error($connect_db));
 	if($qinsert){
-		
-		header("Location: http://localhost/latihan_webphp1/admin/home.php?modul=mod_blog");
+		//ketik proses simpan update berhasil
+		header("Location: http://localhost/latihan_webphp1/admin/home.php?modul=blog");
 	}
 }
 else if(isset($_GET['act']) && ($_GET['act']== "delete")){
+	//jika ada send variabel act=edit, tampil form edit/ubah data
 	$idkey = $_GET['id']; //dapat dari URL
+	$image1=mysqli_query($connect_db,"select  * from mst_blog where id_blog=$idkey");
+	$row1=mysqli_fetch_array($image1);
+	 $image=$row1['gambar'];		
+		unlink("../../assets/img/".$image);
 	$qdelete = mysqli_query($connect_db,"DELETE from mst_blog where id_blog=$idkey")
 				or die(mysqli_error($connect_db));
+	 
 	if($qdelete){
-		header("Location: http://localhost/latihan_webphp1/admin/home.php?modul=mod_blog");
+		header("Location: http://localhost/latihan_webphp1/admin/home.php?modul=blog");
 	}
 }
 ?>
-<!-- aaaaaaaaaaaaaaaaaaaaaaaaaaaaa -->
--- admin/mod_blog/index.php
+++ admin/mod_blog/index.php
@@ -1,209 +1,207 @@
 <?php 
-include_once("blogCtrl.php");
+include_once "blogCTRL.php";
 if(!isset($_GET['act'])){
 //jika tidak ditemukan pengiriman variabel "act"
+?>	
+<?php if(isset($_GET['pesan'])){ ?>
+		<?php if($_GET['pesan']=="sukses") {?>
+			<div class="alert alert-success">
+				berhasil input data 
+			</div>
+		<?php }else if($_GET['pesan']=="gagal") {?>
+			<div class="alert alert-danger">
+				gagal input data
+			</div>
+		<?php }else if($_GET['pesan']=="size") {?>
+			<div class="alert alert-warning">
+				 Ukuran file terlalu besar
+			</div>
+		<?php } else if($_GET['pesan']=="ekstensi") {?>
+			<div class="alert alert-warning">
+				type file anda buka jpg png
+			</div>
+	<?php } ?>
+<?php } ?>
+
+<a href="?modul=blog&act=add" class="btn btn-primary btn-xs mb-1">Tambah Data</a>
+<table class="table table-bordered">
+	<tr>
+		<th>ID blog</th>
+		<th>Judul</th>
+		<th>Kategori</th>
+		<th>isi</th>
+		<th>author</th>
+		<th>date input</th>
+		<th>gambar</th>
+		<th>action</th>
+	</tr>
+	<?php
+	$qry_listmenu = mysqli_query($connect_db,"select * from mst_blog")
+			or die("gagal akses table mst_blog ".mysqli_error($connect_db));
+	while($row = mysqli_fetch_array($qry_listmenu)){
+        $id=$row['id_kategori'];
+        $k1=mysqli_query($connect_db,"select nm_kategori from mst_kategoriblog where id_kategori=$id");
+        if($k2=mysqli_fetch_array($k1)){		
+	?>
+	    <tr>
+		    <td><?= $row['id_blog']; ?></td>
+		    <td><?= $row['judul']; ?></td>
+		    <td><?= $k2['nm_kategori']; ?></td>
+		    <td><?= $row['isi']; ?></td>
+		    <td><?= $row['author']; ?></td>
+		    <td><?= $row['date_input']; ?></td>
+		    <td><img src="../assets/img/<?=$row['gambar'];?>" width="250px"></td>
+		    <td>
+			    <a href="?modul=blog&act=edit&id=<?= $row['id_blog']; ?>" class="btn btn-xs btn-primary p-1"><i
+				    	class="bi bi-pencil-square"></i>Edit</a>
+			    <a href="?modul=blog&act=delete&id=<?= $row['id_blog']; ?>" class="btn btn-xs btn-primary p-1">
+				    <i class="bi bi-trash"></i>Delete</a>
+		    </td>
+	    </tr>
+	<?php
+		}
+    }
+	?>
+</table>
+<?php 
+} //ini penutup if(!isset($_GET['act']))
+else if(isset($_GET['act']) && ($_GET['act']== "add")){
+//ketika di add
 ?>
-<!DOCTYPE html>
-	<html lang="en">
-		<head>
-			<meta charset="UTF-8">
-			<meta http-equiv="X-UA-Compatible" content="IE=edge">
-			<meta name="viewport" content="width=device-width, initial-scale=1.0">
-			<title>Document</title>
-		</head>
-		<body>
-			<a href="?modul=mod_blog&act=add" class="btn btn-primary">Tambah</a>
-			<br><br>
-			<table class="table table-bordered">
-				<tr class="text-center">
-					<th>ID Blog</th>
-					<th>Judul</th>
-					<th>Kategori</th>
-					<th>Isi</th>
-					<th>Author</th>
-					<th>Date Input</th>
-					<th>Gambar</th>
-					<th>Action</th>
-				</tr>
-				<?php
-				$qry_listmenu = mysqli_query($connect_db,"SELECT * FROM mst_blog")
-				or die("gagal akses table mst_kategoriblog ".mysqli_error($connect_db));
-				while($row = mysqli_fetch_array($qry_listmenu)){	
-					$id=$row['id_kategori'];
-					$k1=mysqli_query($connect_db,"SELECT nm_kategori FROM mst_kategoriblog WHERE id_kategori=$id");
-					if($k2=mysqli_fetch_array($k1)){
-						?>
-						<tr>
-							<td><center><?php echo $row['id_blog']; ?></center></td>
-							<td><center><?= $row['judul']; ?></td></center>
-							<td><center><?= $k2['nm_kategori']; ?></center></td>
-							<td>
-								<?=substr ($row['isi'],0,200); ?></td>
-							<td><center><?= $row['author']; ?></td></center>
-							<td><?= $row['date_input']; ?></td>
-							<td><img src="../assets/img/<?= $row['gambar']; ?>" alt="" width="150px"></td>
-							<td>
-								<a href="?modul=mod_blog&act=edit&id=<?= $row['id_kategori']; ?>" class="btn btn-xs btn-primary"> <i class="bi bi-pencil-square" ></i> edit </a>
-								<a href="?modul=mod_kategori&act=delete&id=<?= $row['id_kategori']; ?>" class="btn btn-xs btn-danger p-1"><i class="bi bi-trash"></i>Delete</a>
-							</td>
-						</tr>
-						<?php 
-					}
-				}
-				?>
-			</table>
-				<?php 
-				} //ini penutup if(!isset($_GET['act']))
-				else if(isset($_GET['act']) && ($_GET['act']== "add")){
-					//ketika di add
-					?>
-						<div class="container-fluid">
-							<h3><?php echo $judul; ?></h3>
-							<form action="mod_blog/blogCtrl.php?modul=mod_blog&act=save" method="post" enctype="multipart/form-data">
-							<div class="row mb-1">
-									<label for="" class="col-md-2">judul</label>
-									<div class="col-md-6">
-										<input type="hidden" name="author" value="<?=$_SESSION['userlogin']; ?>">
-										<input type="text" name="judul" id="judul" class="form-control ">
-									</div>
-								</div>
-								<div class="row mb-1">
-									<label for="id_kategori" class="col-md-2">kategori</label>
-									<div class="col-md-6">
-									<select name="id_kategori" id="id_kategori" class="form-select">
-										<option selected disabled>-- pilih kategori --</option>
-										<?php
-										$category=mysqli_query($connect_db,"select * from mst_kategoriblog");
-										while($data=mysqli_fetch_array($category)){
-											?>
-											<option value="<?=$data['id_kategori'];?>"><?=$data['nm_kategori']; ?></option>
-											<?php }?>
-										</select>
-									</div>
-								</div>
-								<div class="row mb-1">
-									<label for="" class="col-md-2">isi</label>
-									<div class="col-md-6">
-										<textarea name="isi" id="editor" rows="5" cols="10" class="form-control "></textarea>
-									</div>
-								</div>
-								<div class="row mb-1">
-									<label for="" class="col-md-2">Tanggal input</label>
-									<div class="col-md-6">
-										<input type="date" name="date_input" id="date_input" class="form-control ">
-									</div>
-								</div>
-								<div class="row mb-1">
-									<label for="" class="col-md-2">Upload gambar</label>
-									<div class="col-md-6">
-										<input type="file" name="urlfile"  id="urlfile" class="form-control">
-									</div>
-								</div>
-								<div class="row">
-									<div class="col-md-2"></div>
-									<div class="col-md-6">
-										<button type="reset" name="btnreset" value="btnbatal" class="btn btn-xs btn-secondary p-1">
-											<i class="bi bi-x-lg"></i>Batal</a></button>
-										<button type="submit" name="btnsimpan" value="btnsimpan" class="btn btn-xs btn-primary p-1">
-											<i class="bi bi-save"></i>Simpan</a></button>
-									</div>
-								</div>
-							</form>
-						</div>
-						<?php }
-						else if(isset($_GET['act']) && ($_GET['act']== "edit")){
-							//ini akan ditampilkan saat klik tombol edit
-							?>
-							<div class="container-fluid">
-								<h3><?php echo $judul ?></h3>
-								<form action="mod_blog/blogCtrl.php?modul=blog&act=update" method="POST">
-								<div class="row mb-1">
-										<label for="" class="col-md-2">judul</label>
-										<div class="col-md-6">
-											<!-- input type hidden ini untuk menyimpan idmenu sebagai key untuk proses update data
-											kenapa di hidden, karena field sbg primary key tidak boleh di edit -->
-											<input type="hidden" name="id_blog" id="id_blog" class="form-control" value="<?= $data['id_blog']; ?>">
-											<input type="text" name="up_judul" id="up_judul" class="form-control" value="<?= $data['judul']; ?>">
-										</div>
-									</div>
-									<div class="row mb-1">
-										<label for="" class="col-md-2">kategori</label>
-										<div class="col-md-6">
-											<select name="up_kategori" id="up_kategori" class="form-select">
-												<?php 
-												$kat1=mysqli_query($connect_db,"select * from mst_kategoriblog");
-												while($updata=mysqli_fetch_array($kat1)){
-													if($updata['id_kategori']===$data['id_kategori']){
-														$select = "selected";
-													} 
-													else{
-														$select = "";
-													}
-												?>
-												<option value="<?=$updata['id_kategori']; ?>"<?= $select; ?>><?=$updata['nm_kategori']; ?></option> 
-												<?php } ?>
-											</select>
-										</div>
-									</div>
-									<div class="row mb-1">
-										<label for="" class="col-md-2">isi</label>
-										<div class="col-md-6">
-											<textarea name="isi" id="isi" class="form-control" ><?= $data['isi']; ?></textarea>
-										</div>
-									</div>
-									<div class="row mb-1">
-										<label for="" class="col-md-2">author</label>
-										<div class="col-md-6">
-											<select name="up_author" id="up_author" class="form-select">
-												<?php 
-												$author1=mysqli_query($connect_db,"select username from mst_user");
-												while($auth=mysqli_fetch_array($author1)){
-													if($auth['username']===$data['author']){
-														$select="selected";
-													}else{
-														$select="";
-													}
-												?>
-													<option value="<?= $auth['username'];?>"<?= $select; ?>><?= $auth['username']; ?></option>
-												<?php } ?>
-											</select>
-										</div>
-									</div>
-									<div class="row mb-1">
-										<label for="" class="col-md-2">date input</label>
-										<div class="col-md-6">
-											<input type="date" name="date_input" id="date_input" class="form-control " value="<?= $data['date_input'] ?>">
-										</div>
-									</div>
-									<div class="row mb-1">
-										<label for="" class="col-md-2">Upload gambar</label>
-										<div class="col-md-6">
-											<input type="file" name="urlfile"  id="urlfile" class="form-control" value="<?= $data['gambar'] ?>">
-										</div>
-									</div>
-									<div class="row">
-										<div class="col-md-2"></div>
-										<div class="col-md-6">
-											<button type="reset" name="btnreset" value="btnbatal" class="btn btn-xs btn-secondary p-1">
-												<i class="bi bi-x-lg"></i> Batal</a></button>
-											<button type="submit" name="btnsimpan" value="btnsimpan" class="btn btn-xs btn-primary p-1">
-												<i class="bi bi-save"></i> Simpan</a></button>
-										</div>
-									</div>
-								</form>
-							</div>
-							<?php
-						}?>
-		</body>
-		<script src="https://cdn.tiny.cloud/1/ctai2l7ettpdz3uyphr0lz4x23v2z3otpascq7sk3miw64e3/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
-		<script>
-		tinymce.init({
-			selector: 'textarea',
-			plugins: 'a11ychecker advcode casechange export formatpainter image editimage linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tableofcontents tinycomments tinymcespellchecker',
-			toolbar: 'a11ycheck addcomment showcomments casechange checklist code export formatpainter image editimage pageembed permanentpen table tableofcontents',
-			toolbar_mode: 'floating',
-			tinycomments_mode: 'embedded',
-			tinycomments_author: 'Author name',
-		});
-  </script>
-</html>
+<div class="container-fluid">
+	<h3><?php echo $judul; ?></h3>
+	<form action="blog/blogCTRL.php?modul=blog&act=save" method="post" enctype="multipart/form-data">
+		<div class="row mb-1">
+			<label for="" class="col-md-2">judul</label>
+			<div class="col-md-6">
+                <input type="hidden" name="author" value="<?=$_SESSION['userlogin']; ?>">
+				<input type="text" name="judul" id="judul" class="form-control ">
+			</div>
+		</div>
+		<div class="row mb-1">
+			<label for="" class="col-md-2">kategori</label>
+			<div class="col-md-6">
+				<select name="kategori" id="kategori" class="form-select">
+                    <option selected disabled>-- pilih kategori --</option>
+                    <?php
+                    $kat=mysqli_query($connect_db,"select * from mst_kategoriblog");
+                    while($kateg=mysqli_fetch_array($kat)){
+                    ?>
+                        <option value="<?=$kateg['id_kategori'];?>"><?=$kateg['nm_kategori']; ?></option>
+                    <?php }?>
+                </select>
+			</div>
+		</div>
+		<div class="row mb-1">
+			<label for="" class="col-md-2">Isi</label>
+			<div class="col-md-6">
+				<textarea name="isi" id="isi" class="form-control " rows="10" cols="100"></textarea>
+			</div>
+		</div>
+		<div class="row mb-1">
+			<label for="" class="col-md-2">date input</label>
+			<div class="col-md-6">
+				<input type="date" name="date_input" id="date_input" class="form-control ">
+			</div>
+		</div>
+		<div class="row mb-1">
+			<label for="" class="col-md-2">Upload Gambar</label>
+			<div class="col-md-6">
+				<input type="file" name="upload" id="upload" class="form-control">
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-2"></div>
+			<div class="col-md-6">
+				<button type="reset" name="btnreset" value="btnbatal" class="btn btn-xs btn-secondary p-1">
+					<i class="bi bi-x-lg"></i> Batal</a></button>
+				<button type="submit" name="btnsimpan" value="btnsimpan" class="btn btn-xs btn-primary p-1">
+					<i class="bi bi-save"></i> Simpan</a></button>
+			</div>
+		</div>
+	</form>
+</div>
+<?php 
+ }
+ else if(isset($_GET['act']) && ($_GET['act']== "edit")){
+?>
+<div class="container-fluid">
+	<h3><?php echo $judul; ?></h3>
+	<form action="blog/blogCTRL.php?modul=blog&act=update" method="post" enctype="multipart/form-data">
+		<div class="row mb-1">
+			<label for="" class="col-md-2">judul</label>
+			<div class="col-md-6">
+				<!-- input type hidden ini untuk menyimpan idmenu sebagai key untuk proses update data
+				kenapa di hidden, karena field sbg primary key tidak boleh di edit -->
+				<input type="hidden" name="id_blog" id="id_blog" class="form-control" value="<?= $data['id_blog']; ?>">
+				<input type="text" name="up_judul" id="up_judul" class="form-control" value="<?= $data['judul']; ?>">
+			</div>
+		</div>
+		<div class="row mb-1">
+			<label for="" class="col-md-2">kategori</label>
+			<div class="col-md-6">
+				<select name="up_kategori" id="up_kategori" class="form-select">
+                    <?php 
+                    $kat1=mysqli_query($connect_db,"select * from mst_kategoriblog");
+                    while($upkateg=mysqli_fetch_array($kat1)){
+                        if($upkateg['id_kategori']===$data['id_kategori']){
+                            $select = "selected";
+                        } 
+                        else{
+                            $select = "";
+                        }
+                    ?>
+                       <option value="<?=$upkateg['id_kategori']; ?>"<?= $select; ?>><?=$upkateg['nm_kategori']; ?></option> 
+                    <?php } ?>
+                </select>
+			</div>
+		</div>
+		<div class="row mb-1">
+			<label for="" class="col-md-2">isi</label>
+			<div class="col-md-6">
+				<textarea name="isi" id="isi" class="form-control" ><?= $data['isi']; ?></textarea>
+			</div>
+		</div>
+		<div class="row mb-1">
+			<label for="" class="col-md-2">author</label>
+			<div class="col-md-6">
+				<select name="up_author" id="up_author" class="form-select">
+                    <?php 
+                    $author1=mysqli_query($connect_db,"select username from mst_user");
+                    while($auth=mysqli_fetch_array($author1)){
+                        if($auth['username']===$data['author']){
+                            $select="selected";
+                        }else{
+                            $select="";
+                        }
+                    ?>
+                        <option value="<?= $auth['username'];?>"<?= $select; ?>><?= $auth['username']; ?></option>
+                    <?php } ?>
+                </select>
+			</div>
+		</div>
+        <div class="row mb-1">
+			<label for="" class="col-md-2">date input</label>
+			<div class="col-md-6">
+				<input type="date" name="date_input" id="date_input" class="form-control " value="<?= $data['date_input'] ?>">
+			</div>
+		</div>
+		<div class="row mb-1">
+			<label for="" class="col-md-2">Upload Gambar</label>
+			<div class="col-md-6">
+				<input type="file" name="upload" id="upload" class="form-control"><img src="../assets/img/<?= $data['gambar'];?>" width="240px">
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-2"></div>
+			<div class="col-md-6">
+				<button type="reset" name="btnreset" value="btnbatal" class="btn btn-xs btn-secondary p-1">
+					<i class="bi bi-x-lg"></i> Batal</a></button>
+				<button type="submit" name="btnsimpan" value="btnsimpan" class="btn btn-xs btn-primary p-1">
+					<i class="bi bi-save"></i> Simpan</a></button>
+			</div>
+		</div>
+	</form>
+</div>
+<script type="text/javascript" src="../../assets/bootstrap5/js/bootstrap.bundle.min.js"></script>  
+<?php } ?>
